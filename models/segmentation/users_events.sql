ab3c33747d79da673bbfb9bda29e24249a6ac428
Tamás Srancsik <tamas.srancsik@gmail.com>
2024-01-26 23:05:36 +0100

    analytics table

--- models/segmentation/users_events.sql
+++ models/segmentation/users_events.sql
@@ -1,3 +1,5 @@
+{{ config(materialized='table') }}
+
 with data1 as (
     select
         user_id,
@@ -5,7 +7,7 @@ with data1 as (
         signup_week,
         signup_month,
         day,
-        document_created,
+        documents_created,
         document_edits,
         document_opens,
         document_shares,
@@ -17,12 +19,6 @@ with data1 as (
         day in (
             0, 1, 7, 28, 91, 365
         )
-        and user_id in (
-        '00067a81-ef46-09ea-c509-bebcb4e23415',
-        '00039149-7429-9321-80d2-69a5527bb791',
-        '0006a3e2-e2a4-27f1-1afe-52a68df3bd54',
-        '00039149-7429-9321-80d2-69a5527bb791'
-    )
 ),
 
 data2 as (
@@ -31,14 +27,49 @@ data2 as (
         signup_date,
         signup_week,
         signup_month,
-        coalesce(case when day = 0 then document_created end, 0) as document_created_0,
-        coalesce(case when day = 1 then document_created end, 0) as document_created_1,
-        coalesce(case when day = 7 then document_created end, 0) as document_created_7,
-        coalesce(case when day = 28 then document_created end, 0) as document_created_28,
-        coalesce(case when day = 91 then document_created end, 0) as document_created_91,
-        coalesce(case when day = 365 then document_created end, 0) as document_created_365
+        max(coalesce(case when day = 0 then documents_created end, 0)) as documents_created_0,
+        max(coalesce(case when day = 1 then documents_created end, 0)) as documents_created_1,
+        max(coalesce(case when day = 7 then documents_created end, 0)) as documents_created_7,
+        max(coalesce(case when day = 28 then documents_created end, 0)) as documents_created_28,
+        max(coalesce(case when day = 91 then documents_created end, 0)) as documents_created_91,
+        max(coalesce(case when day = 365 then documents_created end, 0)) as documents_created_365,
+        max(coalesce(case when day = 0 then document_edits end, 0)) as document_edits_0,
+        max(coalesce(case when day = 1 then document_edits end, 0)) as document_edits_1,
+        max(coalesce(case when day = 7 then document_edits end, 0)) as document_edits_7,
+        max(coalesce(case when day = 28 then document_edits end, 0)) as document_edits_28,
+        max(coalesce(case when day = 91 then document_edits end, 0)) as document_edits_91,
+        max(coalesce(case when day = 365 then document_edits end, 0)) as document_edits_365,
+        max(coalesce(case when day = 0 then document_opens end, 0)) as document_opens_0,
+        max(coalesce(case when day = 1 then document_opens end, 0)) as document_opens_1,
+        max(coalesce(case when day = 7 then document_opens end, 0)) as document_opens_7,
+        max(coalesce(case when day = 28 then document_opens end, 0)) as document_opens_28,
+        max(coalesce(case when day = 91 then document_opens end, 0)) as document_opens_91,
+        max(coalesce(case when day = 365 then document_opens end, 0)) as document_opens_365,
+        max(coalesce(case when day = 0 then document_shares end, 0)) as document_shares_0,
+        max(coalesce(case when day = 1 then document_shares end, 0)) as document_shares_1,
+        max(coalesce(case when day = 7 then document_shares end, 0)) as document_shares_7,
+        max(coalesce(case when day = 28 then document_shares end, 0)) as document_shares_28,
+        max(coalesce(case when day = 91 then document_shares end, 0)) as document_shares_91,
+        max(coalesce(case when day = 365 then document_shares end, 0)) as document_shares_365,
+        max(coalesce(case when day = 0 then comments_created end, 0)) as comments_created_0,
+        max(coalesce(case when day = 1 then comments_created end, 0)) as comments_created_1,
+        max(coalesce(case when day = 7 then comments_created end, 0)) as comments_created_7,
+        max(coalesce(case when day = 28 then comments_created end, 0)) as comments_created_28,
+        max(coalesce(case when day = 91 then comments_created end, 0)) as comments_created_91,
+        max(coalesce(case when day = 365 then comments_created end, 0)) as comments_created_365,
+        max(coalesce(case when day = 0 then reactions_created end, 0)) as reactions_created_0,
+        max(coalesce(case when day = 1 then reactions_created end, 0)) as reactions_created_1,
+        max(coalesce(case when day = 7 then reactions_created end, 0)) as reactions_created_7,
+        max(coalesce(case when day = 28 then reactions_created end, 0)) as reactions_created_28,
+        max(coalesce(case when day = 91 then reactions_created end, 0)) as reactions_created_91,
+        max(coalesce(case when day = 365 then reactions_created end, 0)) as reactions_created_365
     from
         data1
+    group by
+        user_id,
+        signup_date,
+        signup_week,
+        signup_month
 )
 
 select distinct
@@ -46,11 +77,41 @@ select distinct
     signup_date,
     signup_week,
     signup_month,
-    document_created_0 as dc_0,
-    document_created_1 - document_created_0 as dc_1,
-    document_created_7 - document_created_1 as dc_7,
-    document_created_28 - document_created_7 as dc_28,
-    document_created_91 - document_created_28 as dc_91,
-    document_created_365 - document_created_91 as dc_365
+    documents_created_0 as dc_0,
+    documents_created_1 - documents_created_0 as dc_1,
+    documents_created_7 - documents_created_1 as dc_7,
+    documents_created_28 - documents_created_7 as dc_28,
+    documents_created_91 - documents_created_28 as dc_91,
+    documents_created_365 - documents_created_91 as dc_365,
+    document_edits_0 as de_0,
+    document_edits_1 - document_edits_0 as de_1,
+    document_edits_7 - document_edits_1 as de_7,
+    document_edits_28 - document_edits_7 as de_28,
+    document_edits_91 - document_edits_28 as de_91,
+    document_edits_365 - document_edits_91 as de_365,
+    document_opens_0 as do_0,
+    document_opens_1 - document_opens_0 as do_1,
+    document_opens_7 - document_opens_1 as do_7,
+    document_opens_28 - document_opens_7 as do_28,
+    document_opens_91 - document_opens_28 as do_91,
+    document_opens_365 - document_opens_91 as do_365,
+    document_shares_0 as ds_0,
+    document_shares_1 - document_shares_0 as ds_1,
+    document_shares_7 - document_shares_1 as ds_7,
+    document_shares_28 - document_shares_7 as ds_28,
+    document_shares_91 - document_shares_28 as ds_91,
+    document_shares_365 - document_shares_91 as ds_365,
+    comments_created_0 as cc_0,
+    comments_created_1 - comments_created_0 as cc_1,
+    comments_created_7 - comments_created_1 as cc_7,
+    comments_created_28 - comments_created_7 as cc_28,
+    comments_created_91 - comments_created_28 as cc_91,
+    comments_created_365 - comments_created_91 as cc_365,
+    reactions_created_0 as rc_0,
+    reactions_created_1 - reactions_created_0 as rc_1,
+    reactions_created_7 - reactions_created_1 as rc_7,
+    reactions_created_28 - reactions_created_7 as rc_28,
+    reactions_created_91 - reactions_created_28 as rc_91,
+    reactions_created_365 - reactions_created_91 as rc_365
 from
     data2
